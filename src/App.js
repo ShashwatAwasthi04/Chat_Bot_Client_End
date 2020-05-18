9594bdf8437ce6684c3d9506554dabd9d3b0ab99
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-18 16:06:11 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+const App = () => (
+ <Router>
+     <Route path="/" exact component={Join} />
+     <Route path="/chat" exact component={Chat} />
+
+ </Router>
+);
+
+export default App;
