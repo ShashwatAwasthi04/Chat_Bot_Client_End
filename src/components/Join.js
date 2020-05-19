20b99803484ec988df1ae86afab3e2372fd486c3
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-20 01:28:28 +0530

    Added the join Component

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {Link} from 'react-roter-dom';
 const Join = () => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return(
-        <h1>Join</h1>
-    )
+            <div className="joinOuterContainer">
+                <div className="joinInnerContainer">
+                    <h1 className="title">Join the Chat Room</h1>
+                    <div><input placeholder="Your Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
+                    <div><input placeholder="Room Name" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+                    <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                        <buuton className="button mt-20" type="submit">Join In</buuton>
+                    </Link>
+                </div>
+            </div>
+        )
 }
 
 export default Join;
