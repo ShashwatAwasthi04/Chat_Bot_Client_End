c173db56ad6fa99a59fc52fdd1d67caab9973f2b
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-21 01:44:23 +0530

    Added Chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+const END = 'localhost:3000';
+
+const Chat = (location) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+useEffect(() => {
+    const {name,room}= queryString.parse(location.search);
+    
+
+    socket=io(END);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, (error) => {
+        if(error) {
+          alert(error);
+        }
+      });
+},[END, location.search]);
+
     return(
         <h1>Chat</h1>
     )
